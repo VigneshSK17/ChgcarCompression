53dcb82ead905d0dd920a1f3464e6dae5fffd27e
VigneshSK17 <vigsk17@gmail.com>
2024-10-22 13:41:50 -0400

    Added methods to decompress & compute time

--- pyrho_custom.py
+++ pyrho_custom.py
@@ -1,6 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor, wait
 import json
 import sys
+from time import perf_counter
 import numpy as np
 from pymatgen.core.structure import Structure
 from pymatgen.io.cif import CifParser
@@ -23,29 +24,32 @@ def parse_chgcar(chgcar_fn: str):
     structure: Structure = cden.structure
     charge = cden.pgrids["total"]
     mag = cden.pgrids["diff"]
-    lattice = cden.lattice
     data_aug = vasp_cden.as_dict()["data_aug"]
     dims = cden.grid_shape
 
-    return structure, charge, mag, lattice, data_aug, dims
+    return structure, charge, mag, data_aug, dims
 
 # TODO: Time using timeit.timeit not resource.getrusage
 def compress_func(charge: PGrid, mag: PGrid, dims: list[int]):
+
+    time_start = perf_counter()
+
     dims = charge.grid_shape
     compressed_dims = [dim // int(sys.argv[3]) for dim in dims]
 
     charge_compressed = charge.lossy_smooth_compression(compressed_dims, float(sys.argv[4]))
     mag_compressed = mag.lossy_smooth_compression(compressed_dims, float(sys.argv[4]))
 
-    return charge_compressed, mag_compressed
+    time_end = perf_counter()
+
+    return charge_compressed, mag_compressed, time_end - time_start
 
 
-def store_compressed(chgcar_fn: str, charge, mag, structure, lattice, data_aug):
+def store_compressed(chgcar_fn: str, charge, mag, structure, data_aug):
     np.savez_compressed(f"{chgcar_fn}_compressed.npz", charge=charge, mag=mag)
 
     with open(f"{chgcar_fn}_structure.cif", "w") as f:
         f.write(structure.to(fmt="cif"))
-    np.savez_compressed(f"{chgcar_fn}_lattice.npz", lattice=lattice)
     open(f"{chgcar_fn}_data_aug.txt", "w").write(json.dumps(data_aug))
 
 # TODO: Add methods to get compressed data, decompress, and remake CHGCAR
@@ -56,13 +60,15 @@ def retrieve_compressed(chgcar_fn: str):
 
     parser = CifParser(f"{chgcar_fn}_structure.cif")
     structure = parser.parse_structures()[0]
-    lattice = np.load(f"{chgcar_fn}_lattice.npz")["lattice"]
+    lattice = structure.lattice.matrix
     data_aug = json.loads(open(f"{chgcar_fn}_data_aug.txt").read())
 
     return charge_compressed, mag_compressed, structure, lattice, data_aug
 
 
 def decompress_func(data: np.ndarray, lattice: np.ndarray, dims: list[int]):
+    time_start = perf_counter()
+
     pgrid = PGrid(data, lattice)
     up_sample_ratio = dims[0] // data.shape[0]
     pgrid_upscale = pgrid.get_transformed(
@@ -71,9 +77,13 @@ def decompress_func(data: np.ndarray, lattice: np.ndarray, dims: list[int]):
         up_sample=up_sample_ratio
         # up_sample=1
     )
-    return pgrid_upscale
+
+    time_end = perf_counter()
+
+    return pgrid_upscale, time_end - time_start
 
 def remake_chgcar(chgcar_fn: str, charge_pgrid: PGrid, mag_pgrid: PGrid, structure: Structure, data_aug):
+
     cgden = ChargeDensity(pgrids={"total": charge_pgrid, "diff": mag_pgrid}, structure=structure)
 
     chgcar = cgden.to_Chgcar()
@@ -88,19 +98,54 @@ def compress_dir(files: list[str]):
             file_name = file.split(".")[0]
 
             future_parser = executor.submit(parse_chgcar, file)
-            structure, charge, mag, lattice, data_aug, dims = future_parser.result()
+            structure, charge, mag, data_aug, dims = future_parser.result()
 
             orig_values[file_name] = [charge, mag]
 
             future_compressed = executor.submit(compress_func, charge, mag, dims)
 
-            charge_compressed, mag_compressed = future_compressed.result()
+            charge_compressed, mag_compressed, compress_duration = future_compressed.result()
 
-            future_store_compressed = executor.submit(store_compressed, file_name, charge_compressed, mag_compressed, structure, lattice, data_aug)
+            future_store_compressed = executor.submit(store_compressed, file_name, charge_compressed, mag_compressed, structure, data_aug)
+
+            print(f"{file_name} - Compression Duration: {compress_duration} s")
 
     return orig_values
 
-# TODO: Add multithreaded methods to decompress & remake CHGCAR
+def decompress_and_remake_dir(files: list[str]):
+    decompressed_values = {}
+    with ThreadPoolExecutor() as executor:
+        for file in files:
+            paths = file.split("/")
+            only_file_name = paths[-1].split("_")[0] + "_chgcar"
+            file_name = "/".join(paths[:-1]) + "/" + only_file_name
+
+            if file_name in decompressed_values:
+                continue
+
+            future_decompress = executor.submit(retrieve_compressed, file_name)
+            charge_compressed, mag_compressed, structure, lattice, data_aug = future_decompress.result()
+
+            shape = [dim * int(sys.argv[3]) for dim in charge_compressed.shape]
+
+            future_decompress_charge = executor.submit(decompress_func, charge_compressed, lattice, shape)
+            future_decompress_mag = executor.submit(decompress_func, mag_compressed, lattice, shape)
+
+            decompress_charge, decompress_charge_duration = future_decompress_charge.result()
+            decompress_mag, decompress_mag_duration = future_decompress_mag.result()
+
+            decompressed_values[file_name] = [decompress_charge, decompress_mag]
+
+            # Remaking chgcar
+            future_remake_chgcar = executor.submit(remake_chgcar, file_name, decompress_charge, decompress_mag, structure, data_aug)
+
+            future_remake_chgcar.result()
+
+            print(f"{file_name} - Decompression Duration: {decompress_charge_duration + decompress_mag_duration} s\n\t Charge Decompression: {decompress_charge_duration} s\n\t Mag Decompression: {decompress_mag_duration} s")
+
+    return decompressed_values
+
+
 
 def main():
     folder = sys.argv[1]
@@ -114,12 +159,15 @@ def main():
     if method == "compress":
         compressed_values = compress_dir(files)
         for file_name, (charge, mag) in compressed_values.items():
-            print(file_name, charge, mag)
+            print(file_name, charge.grid_shape, mag.grid_shape)
 
     if method == "decompress":
-        pass
+        decompressed_values = decompress_and_remake_dir(files)
+        for file_name, (charge, mag) in decompressed_values.items():
+            print(file_name, charge.grid_shape, mag.grid_shape)
 
     if method == "remake":
+        # TODO: Compress and decompress and then compare MAE
         pass
 
 
